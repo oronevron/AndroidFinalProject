d5ae8884a726b29b5472003fafff1934744fd359
gavelight <ornatan777@gmail.com>
2017-03-25 04:01:36 +0300

    Login/Register/Forgot Password Activities - Working Well. The Design can be changed a little but it looks decent. After Login, we navigate to the main activity where we can display list of trips and stuff...
    I'm not sure about the "Forgot password activity" - I don't think it works so it can be removed later

--- app/src/main/java/com/example/oron/androidfinalproject/MainActivity.java
+++ app/src/main/java/com/example/oron/androidfinalproject/MainActivity.java
@@ -1,13 +1,92 @@
 package com.example.oron.androidfinalproject;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.support.annotation.NonNull;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        // Set authorize listener to handle change in the user state
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                // Get the current user
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                // If user chose to log out
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+                }
+            }
+        };
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // Handle menu action according to item id
+        switch (item.getItemId()) {
+
+            // If user chose to sign out
+            case R.id.sign_out_button:
+
+                // Sign out using firebase api
+                auth.signOut();
+
+                return true;
+
+//            default:
+//                return super.onOptionsItemSelected(item);
+        }
+        return false;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            auth.removeAuthStateListener(authListener);
+        }
+    }
+
 }
