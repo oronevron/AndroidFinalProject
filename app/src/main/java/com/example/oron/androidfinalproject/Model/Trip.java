2b2fae032a32f189b9546c3c0d690817ca13b17c
Oron <oronevron@gmail.com>
2017-04-01 15:13:26 +0300

    Merge branch 'master' of https://github.com/oronevron/AndroidFinalProject
    # Conflicts:
    #	app/src/main/java/com/example/oron/androidfinalproject/Model/Trip.java

--- app/src/main/java/com/example/oron/androidfinalproject/Model/Trip.java
+++ app/src/main/java/com/example/oron/androidfinalproject/Model/Trip.java
@@ -10,9 +10,9 @@ public class Trip {
     private String id;
     private String type;
     private int age_min;
-    private int age_max;
     private int difficulty;
     private String imageName;
+    private double lastUpdated;
 //    private Boolean isChecked = false;
 //    private int year;
 //    private int monthOfYear;
@@ -20,13 +20,13 @@ public class Trip {
 //    private int hourOfDay;
 //    private int minute;
 
-    public Trip(String name, String id, String type, int age_min, int age_max, int difficulty) {// int year, int monthOfYear, int dayOfMonth, int hourOfDay, int minute) {
+    public Trip(String name, String id, String type, int age_min, int difficulty, String imageName) {// int year, int monthOfYear, int dayOfMonth, int hourOfDay, int minute) {
         this.name = name;
         this.id = id;
         this.type = type;
         this.age_min = age_min;
-        this.age_max = age_max;
         this.difficulty = difficulty;
+        this.imageName = imageName;
 //        this.isChecked = isChecked;
 //        this.year = year;
 //        this.monthOfYear = monthOfYear;
@@ -69,14 +69,6 @@ public class Trip {
         this.age_min = age_min;
     }
 
-    public int getAge_max() {
-        return age_max;
-    }
-
-    public void setAge_max(int age_max) {
-        this.age_max = age_max;
-    }
-
     public int getDifficulty() {
         return difficulty;
     }
@@ -91,6 +83,14 @@ public class Trip {
 
     public void setImageName(String imageName) {
         this.imageName = imageName;
+    }
+
+    public Double getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Double lastUpdated) {
+        this.lastUpdated = lastUpdated;
     }
 
     public Map<String, Object> toMap() {
