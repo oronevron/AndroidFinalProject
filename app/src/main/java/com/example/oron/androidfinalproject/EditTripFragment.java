a421288682706120ea9611050d54f9023e0be11e
Oron <oronevron@gmail.com>
2017-04-04 23:03:57 +0300

    Fix the bug of edit is allowed only one time

--- app/src/main/java/com/example/oron/androidfinalproject/EditTripFragment.java
+++ app/src/main/java/com/example/oron/androidfinalproject/EditTripFragment.java
@@ -177,6 +177,7 @@ public class EditTripFragment extends Fragment {
                         Trip tripToEdit = new Trip(name,type,minimalAge,difficulty);
                         tripToEdit.setId(trip.getId());
                         tripToEdit.setImageName(trip.getImageName());
+                        tripToEdit.setUserId(trip.getUserId());
 
                         Model.getInstance().editTrip(tripToEdit, new Model.EditTripListener() {
                             @Override
